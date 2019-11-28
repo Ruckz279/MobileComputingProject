a9b1dcb2e2d03a85fbfde6d6812b4ce718bbf804
Ruckiya <ruckiya.sinorina@gmail.com>
2019-11-28 00:53:51 -0600

    Fixed issue with onActivity result for dashbaord fragment

---
+++ DailyDiet/app/src/main/java/com/example/dailydiet/bottomNavigationUI/dailydiet/DetailIngredientsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dailydiet.bottomNavigationUI.dailydiet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailIngredientsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_ingredients)
+    }
+}
